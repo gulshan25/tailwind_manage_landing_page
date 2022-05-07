d7ee66ba7efb8a16527be04dbb547aa1825a3d21
gulshan25 <g.r.tanny@gmail.com>
2022-05-07 23:59:23 +0600

    tailwind manage landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./*.html"],
+    theme: {
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
+        extend: {
+            colors: {
+                brightRed: "hsl(12, 88%, 59%)",
+            },
+        },
+    },
+    plugins: [],
+};
